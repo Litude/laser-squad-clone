ba75ed52eb11a9b3be0700cd974e3ffc4cd50590
Tommi Räsänen <tommi.rasanen@aalto.fi>
2017-11-22 23:42:58 +0200

    Start refactor of GameScreen

--- src/GameScreen.hpp
+++ src/GameScreen.hpp
@@ -13,27 +13,31 @@ public:
 	GameScreen(sf::RenderWindow &App);
 	virtual int Run(sf::RenderWindow &App);
 private:
-	Game game;
+	void DrawGame(sf::RenderWindow &App);
 	void DrawUI(sf::RenderWindow &App);
+
+	Game game;
 	std::shared_ptr<sf::Font> font;
-	sf::Text textCurTurn;
-	sf::View gameView;
-	sf::View fixedView;
-	sf::RectangleShape interfaceBkg;
-	sf::RectangleShape button;
-	sf::Text textEndTurn;
-	sf::Text textFPS;
-	float lastTime = 0;
-	float currentTime = 0;
-	sf::Clock fpsclock;
-	sf::Clock clock;
-	double timeStep;
-	double timeAccumulator;
-	sf::Sprite characterShapes;
-	std::vector<sf::Sprite> mapTiles;
-	sf::RectangleShape selectedCharacter;
 	std::shared_ptr<sf::Texture> texPlayer1;
 	std::shared_ptr<sf::Texture> texPlayer2;
+	sf::View gameView;
+	sf::View interfaceView;
+	sf::RectangleShape interfaceBkg;
+	sf::RectangleShape buttonEndTurn{ sf::Vector2f(100, 80) };
+	sf::RectangleShape selectedCharacter;
+	sf::Text textCurTurn;
+	sf::Text textEndTurn;
+	sf::Text textFPS;
+	sf::Sprite characterShapes;
+	std::vector<sf::Sprite> mapTiles;
+	sf::Clock fpsclock;
+	sf::Clock clock;
+	float lastTime = 0;
+	float currentTime = 0;
+	double timeStep = 16000;
+	double timeAccumulator = 0;
+
+	bool resized = true;
 };
 
 #endif
