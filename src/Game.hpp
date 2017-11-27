070bcad509ca97ef8d1896c15e38fb40cd98ee0e
Jaakko Soitinaho <jaakko.soitinaho@aalto.fi>
2017-11-27 04:25:15 +0200

    Rename map method calls to grid method calls.

--- src/Game.hpp
+++ src/Game.hpp
@@ -4,18 +4,18 @@
 #include <vector>
 #include "GameCharacter.hpp"
 #include <SFML/System/Vector2.hpp>
-#include "Map.hpp"
+#include "Grid.hpp"
 
 class Game {
 public:
 	Game()									{ playerTurnIdx = 1; }
 	unsigned int							getCurrentPlayer() { return playerTurnIdx; }
 	std::vector<GameCharacter>::iterator	getSelectedCharacter() { return selectedCharacter; }
-	Map&									getMap() { return map; }
+	Grid&									getGrid() { return grid; }
 	void									endTurn();
 	bool									addCharacter(sf::Vector2u position, unsigned int team);
 	void									setSelectedCharacter(std::vector<GameCharacter>::iterator it) { selectedCharacter = it; }
-	void									initializeMap(unsigned int xSize, unsigned int ySize) {map = Map(xSize, ySize);}
+	void									initializeGrid(unsigned int xSize, unsigned int ySize) {grid = Grid(xSize*ySize, xSize);}
 	std::vector<GameCharacter>&				getCharacters() { return characters; }
 	void									characterMoveLeft(std::vector<GameCharacter>::iterator it);
 	void									characterMoveRight(std::vector<GameCharacter>::iterator it);
@@ -25,7 +25,7 @@ public:
 private:
 	unsigned int							playerTurnIdx;
 	std::vector<GameCharacter>::iterator	selectedCharacter;
-	Map										map;
+	Grid									grid;
 	std::vector<GameCharacter>				characters;
 };
 
