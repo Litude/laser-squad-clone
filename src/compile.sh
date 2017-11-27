95a0f45e33253d6d1f31a18dc2e038a2699da296
Jaakko Soitinaho <jaakko.soitinaho@aalto.fi>
2017-11-27 04:38:49 +0200

    Update compile shell script. (Still pretty unreliable)

--- src/compile.sh
+++ src/compile.sh
@@ -2,8 +2,8 @@
 mkdir bin 2> /dev/null
 find . -name "*.o" -print0 | xargs -0 rm 2> /dev/null
 
-g++ -c main.cpp Game.cpp Screen.cpp MainMenuScreen.cpp GameCharacter.cpp Map.cpp Tile.cpp -std=c++0x -g -Wall -Wextra -pthread -Wno-missing-field-initializers -I"$SFML_ROOT/include"
-g++ -o sfml-app main.o Game.o Screen.o MainMenuScreen.o GameCharacter.o Map.o Tile.o -L"$SFML_ROOT/lib" -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c main.cpp Game.cpp Screen.cpp GameScreen.cpp MainMenuScreen.cpp TileMap.cpp GameCharacter.cpp Grid.cpp Tile.cpp -std=c++0x -g -Wall -Wextra -pthread -Wno-missing-field-initializers -I"$SFML_ROOT/include"
+g++ -o sfml-app main.o Game.o Screen.o GameScreen.o MainMenuScreen.o TileMap.o GameCharacter.o Grid.o Tile.o -L"$SFML_ROOT/lib" -lsfml-graphics -lsfml-window -lsfml-system
 
 find . -name "*.o" -print0 | xargs -0 mv -t bin/
 mv sfml-app.exe bin/
