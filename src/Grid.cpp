01d074071d86a9b5f96a46d88de7a238992539bb
Jaakko Soitinaho <jaakko.soitinaho@aalto.fi>
2017-12-03 01:55:53 +0200

    Add better error messages when accessing tiles out of bounds.

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -26,9 +26,9 @@ const Tile& Grid::operator()(sf::Vector2u loc) const {
 
 const Tile& Grid::operator()(int x, int y) const {
 	if (x >= (int) width || y >= (int) getHeight()) {
-		throw std::out_of_range("Tried to access Tile out of bounds");
+		throw std::out_of_range("Tried to access Tile out of bounds at (" + std::to_string(x) + ", " + std::to_string(y) + ")");
 	} else if (x < 0 || y < 0) {
-		throw std::out_of_range("Tried to access Tile at negative coordinates");
+		throw std::out_of_range("Tried to access Tile at negative coordinates (" + std::to_string(x) + ", " + std::to_string(y) + ")");
 	} else {
 		return grid[y*width + x];
 	}
@@ -36,7 +36,7 @@ const Tile& Grid::operator()(int x, int y) const {
 
 const Tile& Grid::operator()(unsigned x, unsigned y) const {
 	if (x >= width || y >= getHeight()) {
-		throw std::out_of_range("Tried to access Tile out of bounds");
+		throw std::out_of_range("Tried to access Tile out of bounds at (" + std::to_string(x) + ", " + std::to_string(y) + ")");
 	}
 	return grid[y*width + x];
 
