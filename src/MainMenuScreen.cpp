bbddd3de8a17dd4e1e2d47f676b0e4ae43e22983
Vili Ketonen <vili.ket@hotmail.com>
2017-12-04 19:29:02 +0200

    Refactor MainMenuScreen class.

--- src/MainMenuScreen.cpp
+++ src/MainMenuScreen.cpp
@@ -1,5 +1,4 @@
 #include "MainMenuScreen.hpp"
-#include "Button.hpp"
 
 MainMenuScreen::MainMenuScreen(void)
 {
@@ -7,50 +6,21 @@ MainMenuScreen::MainMenuScreen(void)
 
 ScreenResult MainMenuScreen::Run(sf::RenderWindow & App)
 {
+	initComponents(App);
 	m_screenResult = ScreenResult::MainMenuScene;
 	sf::Event Event;
-	sf::Texture Texture;
-	sf::Sprite Sprite;
-	sf::Font Font;
-	sf::RectangleShape rs;
-	rs.setFillColor(sf::Color::Red);
-	rs.setSize(sf::Vector2f(20, 20));
 
-	if (!Texture.loadFromFile("img/mainmenu.png"))
-	{
-		std::cerr << "Error loading mainmenu.png" << std::endl;
-		return ScreenResult::Exit;
-	}
-	Sprite.setTexture(Texture);
-
-	if (!Font.loadFromFile("font/ARIALN.TTF"))
-	{
-		std::cerr << "Error loading ARIALN.TTF" << std::endl;
-		return ScreenResult::Exit;
-	}
-
-	std::vector<Button> buttons;
-	Button newgame("New game", Font, sf::Text::Regular, 25, sf::Vector2f(350.f, 250.f));
-	newgame.setCallback([&] {this->openScreen(ScreenResult::NewGameScene); });
-	buttons.push_back(newgame);
-
-	Button mapeditor("Map editor", Font, sf::Text::Regular, 25, sf::Vector2f(350.f, 300.f));
-	mapeditor.setCallback([&] {this->openScreen(ScreenResult::Exit); });
-	buttons.push_back(mapeditor);
-
-	Button quit("Quit", Font, sf::Text::Bold, 25, sf::Vector2f(350.f, 350.f), rs);
-	buttons.push_back(quit);
-/*
-	Button quit("Quit", Font, sf::Text::Regular, 25, sf::Vector2f(350.f, 350.f));
-	quit.setCallback([&] {this->openScreen(ScreenResult::Exit); });
-	buttons.push_back(quit);
-*/
 	auto selectedButtonItem = buttons.begin();
 
 	while (m_screenResult == ScreenResult::MainMenuScene)
 	{
 		while (App.pollEvent(Event))
 		{
+			if (Event.type == sf::Event::Resized)
+			{
+				App.setView(sf::View(sf::FloatRect(0, 0, Event.size.width, Event.size.height)));
+				updateLayout(App);
+			}
 
 			if (Event.type == sf::Event::Closed)
 			{
@@ -85,12 +55,7 @@ ScreenResult MainMenuScreen::Run(sf::RenderWindow & App)
 		}
 
 		selectedButtonItem->setState(state::hovered);
-		App.clear();
-		App.draw(Sprite);
-		for (auto button : buttons) {
-			App.draw(button);
-		}
-		App.display();
+		drawUI(App);
 	}
 
 	return m_screenResult;
@@ -99,4 +64,75 @@ ScreenResult MainMenuScreen::Run(sf::RenderWindow & App)
 void MainMenuScreen::openScreen(ScreenResult res)
 {
 	m_screenResult = res;
+}
+
+void MainMenuScreen::drawUI(sf::RenderWindow &App)
+{
+	App.clear();
+	App.draw(backgroundSprite);
+	App.draw(logoSprite);
+	for (auto button : buttons) {
+		App.draw(button);
+	}
+	App.display();
+}
+
+void MainMenuScreen::updateLayout(sf::RenderWindow & App)
+{
+	backgroundSprite.setScale(
+		App.getView().getSize().x / backgroundSprite.getLocalBounds().width,
+		App.getView().getSize().y / backgroundSprite.getLocalBounds().height);
+	logoSprite.setPosition({ App.getView().getSize().x * 0.5f - logoSprite.getGlobalBounds().width * 0.5f, logoSprite.getGlobalBounds().height * 1.f });
+	float factor = 0.05f;
+	for (auto &button : buttons) {
+		button.setPosition({ App.getView().getSize().x * 0.5f - button.getGlobalBounds().width * 0.5f, logoSprite.getPosition().y + logoSprite.getGlobalBounds().height + App.getView().getSize().y * factor });
+		factor += 0.05f;
+	}
+}
+
+bool MainMenuScreen::initComponents(sf::RenderWindow & App)
+{
+	backgroundTexture = std::make_shared<sf::Texture>(sf::Texture());
+	if (!backgroundTexture->loadFromFile("img/background.png"))
+	{
+		std::cerr << "Error loading background.png" << std::endl;
+		return ScreenResult::Exit;
+	}
+	backgroundSprite.setTexture(*backgroundTexture);
+
+	logoTexture = std::make_shared<sf::Texture>(sf::Texture());
+	if (!logoTexture->loadFromFile("img/logo.png"))
+	{
+		std::cerr << "Error loading logo.png" << std::endl;
+		return ScreenResult::Exit;
+	}
+	logoSprite.setTexture(*logoTexture);
+
+	font = std::make_shared<sf::Font>(sf::Font());
+	if (!font->loadFromFile("font/ARIALN.TTF"))
+	{
+		std::cerr << "Error loading ARIALN.TTF" << std::endl;
+		return ScreenResult::Exit;
+	}
+
+	backgroundSprite.setScale(
+		App.getView().getSize().x / backgroundSprite.getLocalBounds().width,
+		App.getView().getSize().y / backgroundSprite.getLocalBounds().height);
+
+	Button newgame("New game", *font, sf::Text::Regular, 25, sf::Vector2f(350.f, 250.f));
+	newgame.setCallback([&] {this->openScreen(ScreenResult::NewGameScene); });
+	buttons.push_back(newgame);
+
+	Button mapeditor("Map editor", *font, sf::Text::Regular, 25, sf::Vector2f(350.f, 300.f));
+	mapeditor.setCallback([&] {this->openScreen(ScreenResult::Exit); });
+	buttons.push_back(mapeditor);
+
+	sf::RectangleShape rs;
+	rs.setFillColor(sf::Color::White);
+	rs.setSize(sf::Vector2f(120, 40));
+	Button quit("Quit", *font, sf::Text::Bold, 25, sf::Vector2f(350.f, 350.f), rs);
+	quit.setCallback([&] {this->openScreen(ScreenResult::Exit); });
+	buttons.push_back(quit);
+
+	updateLayout(App);
 }
