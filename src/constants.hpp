7f93d68b6389e5853c9872e4e3bf8bbec62caacf
Jaakko Soitinaho <jaakko.soitinaho@aalto.fi>
2017-12-06 21:07:58 +0200

    Add half a tilesize Vec2f constant.

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,5 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <SFML/System/Vector2.hpp>
+
 #define TILESIZE 32
 #define MENUSIZE 200
+#define TILE_CENTER sf::Vector2f(TILESIZE/2, TILESIZE/2)
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
@@ -8,3 +14,5 @@
 
 #define MAX_ITEMS 8
 #define ITEMS_PER_ROW 4
+
+#endif
