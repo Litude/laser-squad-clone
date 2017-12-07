45d42b98104ba39f562dc77e01f627ecd635fd2c
Tommi Räsänen <tommi.rasanen@aalto.fi>
2017-12-07 15:03:31 +0200

    Add AmmoTypes and max ammo for weapons

--- src/Ammo.hpp
+++ src/Ammo.hpp
@@ -1,6 +1,10 @@
+#ifndef LASER_AMMO_HPP
+#define LASER_AMMO_HPP
+
 #include "Item.hpp"
 
 enum AmmoType {
+	Ammo_None,
 	Ammo_9mm_Bullets,
 	Ammo_12mm_Bullets,
 	Ammo_Shotgun_Shells,
@@ -9,17 +13,19 @@ enum AmmoType {
 
 class Ammo : public Item {
 public:
-	Ammo(ItemIcon icon, std::string name, bool stackable, unsigned int amount) : Item(Type_Ammo, icon, name, stackable, amount) {};
+	Ammo(ItemIcon icon, std::string name, bool stackable, unsigned int amount, AmmoType ammotype) : Item(Type_Ammo, icon, name, stackable, amount), m_ammoType(ammotype) {};
 private:
 	AmmoType m_ammoType;
 };
 
 class Ammo9mmBullets : public Ammo {
 public:
-	Ammo9mmBullets() : Ammo(Icon_Ammo_9mm, "9mm Bullets", true, 30) {};
+	Ammo9mmBullets() : Ammo(Icon_Ammo_9mm, "9mm Bullets", true, 30, Ammo_9mm_Bullets) {};
 };
 
 class AmmoShotgunShells : public Ammo {
 public:
-	AmmoShotgunShells() : Ammo(Icon_Ammo_Shells, "Shotgun Shells", true, 20) {};
+	AmmoShotgunShells() : Ammo(Icon_Ammo_Shells, "Shotgun Shells", true, 20, Ammo_Shotgun_Shells) {};
 };
+
+#endif
--- src/Weapon.hpp
+++ src/Weapon.hpp
@@ -6,10 +6,11 @@
 #include <iostream>
 #include <SFML/System/Vector2.hpp>
 #include "Item.hpp"
+#include "Ammo.hpp"
 
 class Weapon : public Item {
 public:
-	Weapon(ItemIcon icon, std::string name, unsigned ap, unsigned loadedAmmo, unsigned damage, unsigned shots, unsigned deviation, int accuracy, unsigned range) : Item(Type_Weapon, icon, name), ap(ap), loadedAmmo(loadedAmmo), damage(damage), shots(shots), deviation(deviation), accuracy(accuracy), range(range) {};
+	Weapon(ItemIcon icon, std::string name, unsigned ap, unsigned loadedAmmo, unsigned damage, unsigned shots, unsigned deviation, int accuracy, unsigned range, AmmoType ammotype) : Item(Type_Weapon, icon, name), ap(ap), loadedAmmo(loadedAmmo), maxAmmo(loadedAmmo), damage(damage), shots(shots), deviation(deviation), accuracy(accuracy), range(range), ammotype(ammotype) {};
 	bool 		reload(unsigned numberOfAmmo);
 	bool		canFire() const;
 	int			fire();
@@ -24,6 +25,7 @@ private:
 	//WeaponType wt
 	unsigned ap;
 	unsigned loadedAmmo;
+	unsigned maxAmmo;
 	unsigned damage;
 	unsigned shots;
 	//maximum number of tiles the shot deviates 
@@ -31,23 +33,24 @@ private:
 
 	int accuracy;
 	unsigned range;
+	AmmoType ammotype;
 };
 
 class Hands : public Weapon {
 public:
-	Hands() : Weapon(Icon_None, "Hands", 1, UINT32_MAX, 3, 1, 1, 100, 1) {};
+	Hands() : Weapon(Icon_None, "Hands", 1, UINT32_MAX, 3, 1, 1, 100, 1, Ammo_None) {};
 	virtual void testInheritance() { std::cout << "Testing hands" << std::endl; };
 };
 
 class Pistol : public Weapon {
 public:
-	Pistol() : Weapon(Icon_Weapon_Pistol, "Pistol", 5, 3, 5, 2, 1, 70, 10) {};
+	Pistol() : Weapon(Icon_Weapon_Pistol, "Pistol", 5, 3, 5, 2, 1, 70, 10, Ammo_9mm_Bullets) {};
 	virtual void testInheritance() override { std::cout << "Testing pistol" << std::endl; };
 };
 
 class Shotgun : public Weapon {
 public:
-	Shotgun() : Weapon(Icon_Weapon_Shotgun, "Shotgun", 5, 3, 5, 2, 1, 70, 10) {};
+	Shotgun() : Weapon(Icon_Weapon_Shotgun, "Shotgun", 5, 3, 5, 2, 1, 70, 10, Ammo_Shotgun_Shells) {};
 	virtual void testInheritance() override { std::cout << "Testing shotgun" << std::endl; };
 };
 
