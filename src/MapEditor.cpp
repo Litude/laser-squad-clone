5844a1fd786176ef79490536a93bfa261bad2a05
Vili Ketonen <vili.ket@hotmail.com>
2017-12-11 12:23:37 +0200

    Cleanup MapEditor from elements that don't belong there.

--- src/MapEditor.cpp
+++ src/MapEditor.cpp
@@ -31,18 +31,6 @@ MapEditor::MapEditor(sf::RenderWindow &App, unsigned int width, unsigned int hei
     interfaceBkg.setFillColor(sf::Color(0, 0, 0, 200));
     textFPS.setFont(*font);
     textFPS.setCharacterSize(12);
-    textCurTurnLabel.setFont(*font);
-    textCurTurnLabel.setCharacterSize(24);
-    textCurTurnLabel.setString("Player");
-    textCurTurnValue.setFont(*font);
-    textCurTurnValue.setCharacterSize(24);
-    textAPLabel.setFont(*font);
-    textAPLabel.setCharacterSize(24);
-    textAPLabel.setString("APs");
-    textAPValue.setFont(*font);
-    textAPValue.setCharacterSize(24);
-    textMouseMode.setFont(*font);
-    textMouseMode.setCharacterSize(24);
     
     backgroundTexture = std::make_shared<sf::Texture>(sf::Texture());
     if (!backgroundTexture->loadFromFile("img/background.png"))
@@ -58,39 +46,14 @@ MapEditor::MapEditor(sf::RenderWindow &App, unsigned int width, unsigned int hei
     buttonExit = Button("Exit to Main Menu", *font, sf::Text::Regular, 25, sf::Vector2f(0.f, 0.f), rs);
     buttonExit.setCallback([&] { this->exitToMainMenu(); });
     
-    buttonEndTurn = Button("End turn", *font, sf::Text::Regular, 25, sf::Vector2f(0.f, 0.f), rs);
-    buttonEndTurn.setCallback([&] { this->endTurn(); });
-    
     rs.setSize(sf::Vector2f(50, 20));
     
-    buttonPickupItem = Button("Pick", *font, sf::Text::Regular, 16, sf::Vector2f(0.f, 0.f), rs);
-    buttonPickupItem.setCallback([&] { this->pickupItem(); });
-    
-    buttonDropItem = Button("Drop", *font, sf::Text::Regular, 16, sf::Vector2f(0.f, 0.f), rs);
-    buttonDropItem.setCallback([&] { this->dropItem(); });
-    
-    buttonEquipItem = Button("Equip", *font, sf::Text::Regular, 16, sf::Vector2f(0.f, 0.f), rs);
-    buttonEquipItem.setCallback([&] { this->equipItem(); });
-    
     //Game drawing initialization
-    selectedCharacter = sf::RectangleShape(sf::Vector2f(TILESIZE, TILESIZE));
-    selectedCharacter.setOutlineColor(sf::Color::Yellow);
-    selectedCharacter.setOutlineThickness(2.0f);
-    selectedCharacter.setFillColor(sf::Color::Transparent);
-    
-    selectedItem = sf::RectangleShape(sf::Vector2f(TILESIZE, TILESIZE));
-    selectedItem.setOutlineColor(sf::Color::Yellow);
-    selectedItem.setOutlineThickness(2.0f);
-    selectedItem.setFillColor(sf::Color::Transparent);
-    
     selectedTile = sf::RectangleShape(sf::Vector2f(TILESIZE, TILESIZE));
     selectedTile.setOutlineColor(sf::Color::Yellow);
     selectedTile.setOutlineThickness(2.0f);
     selectedTile.setFillColor(sf::Color::Transparent);
     selectedTile.setPosition(97,97);
-    
-    equippedItem = sf::RectangleShape(sf::Vector2f(TILESIZE, TILESIZE));
-    equippedItem.setFillColor(sf::Color::Red);
     
     texPlayer1 = std::make_shared<sf::Texture>(sf::Texture());
     if (!texPlayer1->loadFromFile("img/character1_sheet.png")) {
@@ -119,39 +82,11 @@ MapEditor::MapEditor(sf::RenderWindow &App, unsigned int width, unsigned int hei
         character.setAnimationManager(animManager);
     }
     
-    //Ray tracing line for shooting
-    rayLine[0].color = sf::Color::Red;
-    rayLine[1].color = sf::Color::Blue;
-    
-    // Create a render-texture to draw visible tiles based on line of sight
-    renderTexture_visibleTiles = std::make_shared<sf::RenderTexture>();
-    if (!renderTexture_visibleTiles->create(game.getGrid().getWidth() * TILESIZE, game.getGrid().getHeight() * TILESIZE))
-    {
-        std::cout << "Could not initialize render texture\n";
-    }
-    
-    // Create helper shape to draw line of sight on the render texture
-    visibleTileShape = sf::RectangleShape(sf::Vector2f(TILESIZE, TILESIZE));
-    visibleTileShape.setOutlineColor(sf::Color::Black);
-    visibleTileShape.setOutlineThickness(2.0f);
-    visibleTileShape.setFillColor(sf::Color(150,150,150,255));
-    
-    // Create shader for line of sight rendering
-    shader = new sf::Shader();
-    if (!shader->loadFromFile("img/lineofsight_shader.frag", sf::Shader::Fragment))
-    {
-        std::cout << "Could not load 'img/lineofsight_shader.frag'\n";
-    }
-    
     texItems = std::make_shared<sf::Texture>(sf::Texture());
     if (!texItems->loadFromFile("img/tileset_items.png")) {
         std::cout << "Could not load 'img/tileset_items.png'\n";
     }
-    inventoryItems.resize(MAX_ITEMS);
-    
-    for (unsigned int i = 0; i < MAX_ITEMS; i++) {
-        inventoryItems[i].setTexture(*texItems);
-    }
+   
     updateLayout(App);
     
     // Center gameview
@@ -189,10 +124,6 @@ ScreenResult MapEditor::Run(sf::RenderWindow & App)
             }
             // Update buttons
             buttonExit.update(event, App);
-            buttonEndTurn.update(event, App);
-            buttonPickupItem.update(event, App);
-            buttonDropItem.update(event, App);
-            buttonEquipItem.update(event, App);
             //Handle keyboard input
             
             int xx=32;
@@ -328,63 +259,8 @@ ScreenResult MapEditor::Run(sf::RenderWindow & App)
                 }
                 
             }
-            //Handle mouse input
-            if (event.type == sf::Event::MouseButtonReleased) {
-                unsigned int menuSize = App.getSize().x / 4;
-                if (event.mouseButton.x >= App.getSize().x - menuSize) {
-                    //Clicked on the menubar
-                    if (event.mouseButton.button == 0) {
-                        if (game.getSelectedCharacter() != game.getCharacters().end()) {
-                            for (unsigned int i = 0; i < MAX_ITEMS; i++) {
-                                if (inventoryItems[i].getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(App))) && game.getSelectedCharacter()->getInventory()[i]->getType() != Type_None) {
-                                    game.getSelectedCharacter()->setSelectedItemIndex(i);
-                                    break;
-                                }
-                                else {
-                                    game.getSelectedCharacter()->setSelectedItemIndex(-1);
-                                }
-                            }
-                        }
-                    }
-                }
-                else {
-                    //Clicked on gamescreen
-                    //In shoot mode
-                    if (mouseMode == MouseMode2::shoot && game.getSelectedCharacter() != game.getCharacters().end()) {
-                        auto gc = game.getSelectedCharacter();
-                        auto tiles = game.characterShoot(gc, getClickedTilePosition(App, sf::Mouse::getPosition(App), gameView));
-                        for (auto dest : tiles) {
-                            addProjectile(gc->getEquipped(), gc->getPosition(), dest);
-                        }
-                        
-                        std::cout << "PROJS:" << std::endl;
-                        for (auto proj : activeProjectiles) {
-                            std::cout << proj;
-                        }
-                    }
-                    //In select mode
-                    else {
-                        for (auto it = game.getCharacters().begin(); it != game.getCharacters().end(); ++it) {
-                            if (it->getTeam() == game.getCurrentPlayer() && it->getPosition() == getClickedTilePosition(App, sf::Mouse::getPosition(App), gameView)) {
-                                game.setSelectedCharacter(it); //Select clicked character
-                                break;
-                            }
-                            else {
-                                game.setSelectedCharacter(game.getCharacters().end());
-                            }
-                        }
-                    }
-                }
-            }
             if (event.type == sf::Event::Resized) {
                 updateLayout(App);
-            }
-            if (event.type == sf::Event::MouseMoved && mouseMode == MouseMode2::shoot && game.getSelectedCharacter() != game.getCharacters().end()) {
-                auto gc = game.getSelectedCharacter();
-                auto target = getClickedTilePosition(App, sf::Mouse::getPosition(App), gameView);
-                auto origin = gc->getPosition();
-                rayLine[0].position = Util::mapToPixels(origin);
-                rayLine[1].position = Util::mapToPixels(game.traceFromCharacter(gc, target, true));
             }
         }
         
@@ -439,10 +315,6 @@ void MapEditor::DrawGame(sf::RenderWindow &App) {
         //}
         sf::Sprite characterShape;
         characterShape.setPosition(it->getRenderPosition().x, it->getRenderPosition().y);
-        if (it == game.getSelectedCharacter()) {
-            selectedCharacter.setPosition(it->getRenderPosition().x,it->getRenderPosition().y);
-            App.draw(selectedCharacter);
-        }
         if (it->getTeam() == 1) {
             characterShape.setTexture(*texPlayer1);
         }
@@ -460,21 +332,8 @@ void MapEditor::DrawUI(sf::RenderWindow &App) {
     updateUIComponents(App);
     
     //Interface elements that always need to be updated
-    textCurTurnValue.setString(std::to_string(game.getCurrentPlayer()));
-    
-    std::string mm = (mouseMode == MouseMode2::shoot) ? "SHOOT MODE" : "SELECT MODE";
-    textMouseMode.setString(mm);
     
     //TODO: Process selected character attributes here and draw them on the interface...
-    if (game.getSelectedCharacter() != game.getCharacters().end()) {
-        textAPValue.setString(std::to_string(game.getSelectedCharacter()->getActionPoints()) + '/' + std::to_string(game.getSelectedCharacter()->getMaxActionPoints()));
-        
-        if (game.getSelectedCharacter()->getSelectedWeaponIndex() != -1) {
-            unsigned int margin = 10;
-            unsigned int offset = menuSize / 4;
-            equippedItem.setPosition((App.getSize().x - menuSize + margin) + ((game.getSelectedCharacter()->getSelectedWeaponIndex() % ITEMS_PER_ROW) * offset), 330 + (game.getSelectedCharacter()->getSelectedWeaponIndex() / ITEMS_PER_ROW) * (offset));
-        }
-    }
     
     currentTime = fpsclock.getElapsedTime().asMicroseconds() / 1000000.0f;
     float fps = 1.f / (currentTime - lastTime);
@@ -486,31 +345,8 @@ void MapEditor::DrawUI(sf::RenderWindow &App) {
     App.draw(backgroundSprite);
     App.draw(interfaceBkg); //Must always be first since it covers the whole area and will hide anything "under" it
     
-    // Draw action points
-    if (game.getSelectedCharacter() != game.getCharacters().end()) {
-        App.draw(textAPLabel);
-        App.draw(textAPValue);
-        App.draw(buttonPickupItem);
-        App.draw(buttonDropItem);
-        App.draw(buttonEquipItem);
-        if (game.getSelectedCharacter()->getSelectedWeaponIndex() != -1) App.draw(equippedItem);
-        // Draw items
-        for (unsigned int i = 0; i < MAX_ITEMS; i++) {
-            inventoryItems[i].setTextureRect(sf::IntRect(game.getSelectedCharacter()->getInventory()[i]->getIcon() * TILESIZE, 0, TILESIZE, TILESIZE));
-            App.draw(inventoryItems[i]);
-            if (game.getSelectedCharacter()->getSelectedItemIndex() == i) {
-                selectedItem.setPosition(inventoryItems[i].getPosition());
-                App.draw(selectedItem);
-            }
-        }
-    }
-    
     App.draw(textFPS);
-    App.draw(textMouseMode);
     App.draw(buttonExit);
-    App.draw(buttonEndTurn);
-    App.draw(textCurTurnLabel);
-    App.draw(textCurTurnValue);
 }
 
 void MapEditor::updateLayout(sf::RenderWindow & App)
@@ -545,38 +381,6 @@ void MapEditor::updateLayout(sf::RenderWindow & App)
     rs.setSize(sf::Vector2f(menuSize - margin, 40));
     buttonExit.setRectangleShape(rs);
     
-    // End turn button
-    //buttonEndTurn.setPosition(sf::Vector2f(menuCenterX, buttonExit.getPos().y - buttonExit.getGlobalBounds().height / 2 - buttonEndTurn.getGlobalBounds().height / 2 - margin));
-    //rs.setFillColor(sf::Color::White);
-    //rs.setSize(sf::Vector2f(menuSize - margin, 40));
-    //buttonEndTurn.setRectangleShape(rs);
-    
-    // Pick up item button
-    rs.setSize(sf::Vector2f(menuSize / 3 - margin, 40));
-    buttonPickupItem.setRectangleShape(rs);
-    buttonPickupItem.setPosition(sf::Vector2f(menuCenterX - menuSize / 2 + buttonPickupItem.getGlobalBounds().width / 2 + margin, 300));
-    
-    // Drop item button
-    rs.setSize(sf::Vector2f(menuSize / 3 - margin, 40));
-    buttonDropItem.setRectangleShape(rs);
-    buttonDropItem.setPosition(sf::Vector2f(menuCenterX, 300));
-    
-    // Equip item button
-    rs.setSize(sf::Vector2f(menuSize / 3 - margin, 40));
-    buttonEquipItem.setRectangleShape(rs);
-    buttonEquipItem.setPosition(sf::Vector2f(App.getSize().x - buttonEquipItem.getGlobalBounds().width / 2 - margin, 300));
-    
-    // Inventory item positions
-    unsigned int offset = menuSize / 4;
-    for (unsigned int i = 0; i < MAX_ITEMS; i++) {
-        inventoryItems[i].setTextureRect(sf::IntRect(0, 0, TILESIZE, TILESIZE));
-        inventoryItems[i].setScale(sf::Vector2f(menuSize / ITEMS_PER_ROW / TILESIZE, menuSize / ITEMS_PER_ROW / TILESIZE));
-        inventoryItems[i].setPosition((App.getSize().x - menuSize + margin) + ((i % ITEMS_PER_ROW) * offset), 330 + (i / ITEMS_PER_ROW) * (offset));
-    }
-    
-    selectedItem.setSize(sf::Vector2f(inventoryItems[0].getGlobalBounds().width, inventoryItems[0].getGlobalBounds().height));
-    equippedItem.setSize(sf::Vector2f(inventoryItems[0].getGlobalBounds().width, inventoryItems[0].getGlobalBounds().height));
-    
     updateUIComponents(App);
 }
 
@@ -588,47 +392,6 @@ void MapEditor::updateUIComponents(sf::RenderWindow & App)
     
     // FPS counter text
     textFPS.setPosition(menuCenterX - menuSize / 2 + margin, 0);
-    
-    // AP text
-    textAPLabel.setPosition(menuCenterX - menuSize / 2 + margin, 100);
-    textAPValue.setPosition(menuCenterX + menuSize / 2 - margin - textAPValue.getGlobalBounds().width, 100);
-    
-    // Current turn text
-    textCurTurnLabel.setPosition(menuCenterX - menuSize / 2 + margin, 50);
-    textCurTurnValue.setPosition(menuCenterX + menuSize / 2 - margin - textCurTurnValue.getGlobalBounds().width, 50);
-    
-    // Mousemode text
-    textMouseMode.setPosition(menuCenterX - textMouseMode.getGlobalBounds().width / 2, 150);
-}
-
-void MapEditor::endTurn() {
-    mouseMode = MouseMode2::select;
-    rayLine[0].position = sf::Vector2f(0, 0);
-    rayLine[1].position = sf::Vector2f(0, 0);
-    game.endTurn();
-}
-
-void MapEditor::pickupItem() {
-    if (game.getSelectedCharacter() != game.getCharacters().end()) {
-        if (game.characterPickUpItem(game.getSelectedCharacter())) {
-            tileMap->updateTile(game.getSelectedCharacter()->getPosition());
-        }
-    }
-}
-
-void MapEditor::dropItem() {
-    if (game.getSelectedCharacter() != game.getCharacters().end()) {
-        if (game.getSelectedCharacter()->getSelectedItemIndex() != -1) {
-            game.characterDropItem(game.getSelectedCharacter());
-            tileMap->updateTile(game.getSelectedCharacter()->getPosition());
-        }
-    }
-}
-
-void MapEditor::equipItem() {
-    if (game.getSelectedCharacter() != game.getCharacters().end()) {
-        game.getSelectedCharacter()->useSelected();
-    }
 }
 
 sf::Vector2u MapEditor::getClickedTilePosition(const sf::RenderWindow& App, const sf::Vector2i& point, const sf::View& view) const {
@@ -636,12 +399,6 @@ sf::Vector2u MapEditor::getClickedTilePosition(const sf::RenderWindow& App, cons
     clickedTile.x /= TILESIZE;
     clickedTile.y /= TILESIZE;
     return clickedTile;
-}
-
-void MapEditor::addProjectile(std::shared_ptr<Weapon> weapon, sf::Vector2u world_origin, sf::Vector2u world_destination) {
-    ItemIcon wt = weapon->getIcon();
-    Projectile p(wt, Util::mapToPixels(world_origin), Util::mapToPixels(world_destination));
-    activeProjectiles.push_back(p);
 }
 
 void MapEditor::zoomViewAt(sf::Vector2i pixel, sf::RenderWindow& window, sf::View &view, float zoom)
--- src/MapEditor.hpp
+++ src/MapEditor.hpp
@@ -36,17 +36,11 @@ private:
 private:
     void exitToMainMenu();
     void zoomViewAt(sf::Vector2i pixel, sf::RenderWindow& window, sf::View &view, float zoom);
-    void endTurn();
-    void pickupItem();
-    void dropItem();
-    void equipItem();
     void DrawGame(sf::RenderWindow &App);
     void DrawUI(sf::RenderWindow &App);
     void updateLayout(sf::RenderWindow & App);
     void updateUIComponents(sf::RenderWindow & App);
     sf::Vector2u getClickedTilePosition(const sf::RenderWindow& App, const sf::Vector2i& point, const sf::View& view) const;
-    void DrawVisibleArea(sf::RenderWindow &App, std::vector<sf::Vector2u> visibleTiles);
-    void addProjectile(std::shared_ptr<Weapon> weapon, sf::Vector2u world_origin, sf::Vector2u world_destination);
     
     Game game;
     std::shared_ptr<TileMap> tileMap;
@@ -60,37 +54,16 @@ private:
     sf::View interfaceView;
     
     sf::RectangleShape interfaceBkg;
-    Button buttonEndTurn;
-    Button buttonPickupItem;
-    Button buttonDropItem;
-    Button buttonEquipItem;
     Button buttonExit;
-    sf::RectangleShape selectedCharacter;
     sf::RectangleShape selectedTile;
-    sf::RectangleShape selectedItem;
-    sf::RectangleShape equippedItem;
-    sf::Text textCurTurnLabel;
-    sf::Text textCurTurnValue;
     sf::Text textFPS;
-    sf::Text textAPLabel;
-    sf::Text textAPValue;
-    sf::Text textMouseMode;
     
-    int rayIncr = 1;
-    sf::VertexArray rayLine{ sf::Lines, 2 };
-    
-    std::vector<sf::Sprite> mapTiles;
     sf::Clock fpsclock;
     sf::Clock clock;
     float lastTime = 0;
     float currentTime = 0;
     double timeStep = 16000;
     double timeAccumulator = 0;
-    std::shared_ptr<sf::RenderTexture> renderTexture_visibleTiles;
-    sf::RectangleShape visibleTileShape;
-    sf::Shader* shader;
-    std::vector<sf::Sprite> inventoryItems;
-    std::vector<Projectile> activeProjectiles;
     
     MouseMode2::Mode2 mouseMode = MouseMode2::select;
 };
