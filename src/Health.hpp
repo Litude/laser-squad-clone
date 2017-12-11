e17e5611ceaddd566822520c665e533bc4863f9b
Tommi Räsänen <tommi.rasanen@aalto.fi>
2017-12-11 10:59:09 +0200

    Refactor Item and Inventory logic to work mostly the same for stackable and unstackable items

--- src/Health.hpp
+++ src/Health.hpp
@@ -6,7 +6,7 @@
 
 class Health : public Item {
 public:
-	Health(unsigned int healingAmount, ItemIcon icon, std::string name) : Item(Type_Health, icon, name), m_healingAmount(healingAmount) {};
+	Health(unsigned int healingAmount, ItemIcon icon, std::string name, bool stackable, unsigned int amount) : Item(Type_Health, icon, name, stackable, true, amount), m_healingAmount(healingAmount) {};
 	unsigned int getHealingAmount() const { return m_healingAmount; }
 private:
 	unsigned int m_healingAmount;
@@ -14,13 +14,13 @@ private:
 
 class HealthPackSmall : public Health {
 public:
-	HealthPackSmall() : Health(2, Icon_Health_Small, "Small Health Pack") {};
+	HealthPackSmall() : Health(2, Icon_Health_Small, "Small Health Pack", true, 1) {};
 };
 
 
 class HealthPackLarge : public Health {
 public:
-	HealthPackLarge() : Health(5, Icon_Health_Large, "Large Health Pack") {};
+	HealthPackLarge() : Health(5, Icon_Health_Large, "Large Health Pack", false, 1) {};
 };
 
 #endif
