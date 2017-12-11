c47502566a5ce14143175d83a8d9a9f704d43b9f
Jaakko Soitinaho <jaakko.soitinaho@aalto.fi>
2017-12-12 00:51:05 +0200

    Implement character and item loading from JSON.

--- src/GridLoader.hpp
+++ src/GridLoader.hpp
@@ -6,20 +6,52 @@
 #include <typeinfo>
 #include <map>
 #include <string>
+#include <memory>
 #include "libs/json.hpp"
 #include "Grid.hpp"
 #include "Tile.hpp"
 #include "Item.hpp"
 #include "Game.hpp"
 #include "Exceptions.hpp"
+#include "Health.hpp"
+#include "Weapon.hpp"
+#include "Ammo.hpp"
 
 namespace jreader {
 
+	typedef Item* (*item_create)();
 	static std::map<std::string, TileBlock> block = {{"wall", wall}, {"tree", tree}, {"bush", bush}, {"air", air}};
 	static std::map<std::string, TileGround> ground = {{"grass", grass}, {"dirt", dirt}, {"wood", wood}, {"stone", stone}, {"metal", metal}, {"black", black}};
-	//static const std::map<std::string, ItemIcon> itemCreator = {{"Small Health Pack"}, {"Large Health Pack"}}
 	static std::vector<std::string> ground_str = {"black", "dirt", "grass", "wood", "stone", "metal"};
 	static std::vector<std::string> block_str = {"air", "wall", "tree", "bush"};
+
+	inline std::shared_ptr<Item> create_item(std::string item_str) {
+		if (item_str == "Small Health Pack") {
+			return std::make_shared<HealthPackSmall>();
+		} else if (item_str == "Large Health Pack") {
+			return std::make_shared<HealthPackLarge>();
+		} else if (item_str == "Pistol") {
+			return std::make_shared<Pistol>();
+		} else if (item_str == "Shotgun") {
+			return std::make_shared<Shotgun>();
+		} else if (item_str == "Uzi") {
+			return std::make_shared<Uzi>();
+		} else if (item_str == "Rifle") {
+			return std::make_shared<Rifle>();
+		} else if (item_str == "Double Barrel") {
+			return std::make_shared<DoubleBarrel>();
+		} else if (item_str == "9mm Bullets") {
+			return std::make_shared<Ammo9mmBullets>();
+		} else if (item_str == "12mm Bullets") {
+			return std::make_shared<Ammo12mmBullets>();
+		} else if (item_str == "Rockets") {
+			return std::make_shared<AmmoRockets>();
+		} else if (item_str == "Shotgun Shells") {
+			return std::make_shared<AmmoShotgunShells>();
+		} else {
+			return nullptr;
+		}
+	}
 
 	// Get with default value
 	template <typename T>
@@ -37,14 +69,23 @@ namespace jreader {
 		if (j["block"] == nullptr) return Tile();
 		auto gr = get(ground, j["ground"], grass);
 		auto bl = get(block, j["block"], air);
-		return Tile(gr, bl);
+		Tile tile(gr, bl);
+
+		for (auto& jitem : j["items"]) {
+			if (jitem.is_string()) {
+				auto item_ptr = create_item(jitem);
+				if (item_ptr)
+					tile.addItem(item_ptr);
+			}
+		}
+		return tile;
 	}
 
 	inline nlohmann::json createTile(Tile& tile) {
 		nlohmann::json jtile;
 		jtile["ground"] = ground_str[tile.getGround()];
 		jtile["block"] = block_str[tile.getBlock()];
-		for (auto item : tile.getItems()) {
+		for (auto& item : tile.getItems()) {
 			jtile["items"].push_back((*item).getName());
 		}
 		return jtile;
@@ -71,7 +112,7 @@ namespace jreader {
 		
 		unsigned u = 0;
 		
-		for (auto j : jgrid["tiles"]) {
+		for (auto& j : jgrid["tiles"]) {
 			if (u > mapSize) break;
 			Tile t = createTile(j);
 			g[u] = t;
@@ -83,7 +124,39 @@ namespace jreader {
 		return g;
 	}
 
-	inline Grid loadJSON(std::string filepath) {
+	inline std::unique_ptr<GameCharacter> createCharacter(nlohmann::json& jchar) {
+		if (jchar["position"].is_array() && jchar["position"].size() == 2) {
+			if (jchar["position"][0].is_number_unsigned() 
+				&& jchar["position"][1].is_number_unsigned() 
+				&& jchar["team"].is_number_unsigned()) {
+				
+				unsigned x = jchar["position"][0];
+				unsigned y = jchar["position"][1];
+				unsigned team = jchar["team"];
+				std::cout << "pos: (" << x << ", " << y << ")" << std::endl;
+				std::unique_ptr<GameCharacter> gc_ptr(new GameCharacter(sf::Vector2u(x, y), team));// GameCharacter gc(sf::Vector2u(x, y), team);
+				return gc_ptr;
+			}
+		}
+		throw JSONLoadException();
+	}
+
+	inline void loadCharacters(Game& game, nlohmann::json& characters) {
+		if (characters.is_array()) {
+			for (auto& jchar : characters) {
+				try {
+					auto gc_ptr = createCharacter(jchar);
+					game.addCharacter(*gc_ptr);
+				} catch (JSONLoadException& e) {
+					continue;
+				}
+			}
+		} else {
+			throw JSONLoadException();
+		}
+	}
+
+	inline std::shared_ptr<Game> loadJSON(std::string filepath) {
 		nlohmann::json jloader;
 		std::ifstream is;
 		is.exceptions( std::ifstream::failbit | std::ifstream::badbit );
@@ -103,13 +176,13 @@ namespace jreader {
 			throw JSONLoadException();
 		}
 
-		Grid g = loadGrid(jloader["map"]);
-
 		Game game;
-
+		Grid g = loadGrid(jloader["map"]);
 		game.setGrid(g);
 
-		return g;
+		loadCharacters(game, jloader["characters"]);
+
+		return std::make_shared<Game>(game);
 	}
 
 	inline bool writeJSON(Game& game, std::string filename) {
@@ -120,16 +193,19 @@ namespace jreader {
 		writer["map"]["tiles"] = nlohmann::json::array();
 		writer["characters"] = nlohmann::json::array();
 
-		for (auto tile : game.getGrid()) {
+		for (auto& tile : game.getGrid()) {
 			nlohmann::json jtile = createTile(tile);
 			writer["map"]["tiles"].push_back(jtile);
 		}
-		for (auto gc : game.getCharacters()) {
+		for (auto& gc : game.getCharacters()) {
 			nlohmann::json jchar;
 			jchar["team"] = gc.getTeam();
 			jchar["health"] = gc.getHitpoints();
 			jchar["inventory"] = nlohmann::json::array();
-			for (auto item : gc.getInventory()) {
+			jchar["position"] = nlohmann::json::array();
+			jchar["position"].push_back(gc.getPosition().x);
+			jchar["position"].push_back(gc.getPosition().y);
+			for (auto& item : gc.getInventory()) {
 				jchar["inventory"].push_back(item->getName());
 			}
 			writer["characters"].push_back(jchar);
@@ -142,7 +218,7 @@ namespace jreader {
 			os << writer;
 			os.close();
 		} catch (std::ofstream::failure& e) {
-			throw JSONLoadException();
+			throw JSONWriteException();
 		}
 		return true;
 	}	
