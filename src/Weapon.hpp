6c755c42872c7cd021857cfb6dd3cb3cff58dc3f
Tommi Räsänen <tommi.rasanen@aalto.fi>
2017-12-13 17:32:50 +0200

    Add logic for throwable weapons (Grenade and throwable knife)

--- src/Weapon.hpp
+++ src/Weapon.hpp
@@ -10,7 +10,8 @@
 
 class Weapon : public Item {
 public:
-	Weapon(ItemIcon icon, std::string name, unsigned ap, unsigned loadedAmmo, unsigned damage, unsigned shots, unsigned deviation, int accuracy, unsigned range, AmmoType ammoType, unsigned shot_delay=100, unsigned area=0) : Item(Type_Weapon, icon, name), ap(ap), loadedAmmo(loadedAmmo), maxAmmo(loadedAmmo), damage(damage), shots(shots), deviation(deviation), accuracy(accuracy), range(range), ammoType(ammoType), shot_delay(shot_delay), aoe(area) {};
+	Weapon(ItemIcon icon, std::string name, unsigned ap, unsigned loadedAmmo, unsigned damage, unsigned shots, unsigned deviation, int accuracy, unsigned range, AmmoType ammoType, unsigned shot_delay = 100, unsigned area = 0) : Item(Type_Weapon, icon, name), ap(ap), loadedAmmo(loadedAmmo), maxAmmo(loadedAmmo), damage(damage), shots(shots), deviation(deviation), accuracy(accuracy), range(range), ammoType(ammoType), shot_delay(shot_delay), aoe(area) {};
+	Weapon(ItemIcon icon, std::string name, bool stackable, bool consumable, unsigned amount, unsigned ap, unsigned loadedAmmo, unsigned damage, unsigned shots, unsigned deviation, int accuracy, unsigned range, AmmoType ammoType, unsigned shot_delay = 100, unsigned area = 0) : Item(Type_Weapon, icon, name, stackable, consumable, amount), ap(ap), loadedAmmo(loadedAmmo), maxAmmo(loadedAmmo), damage(damage), shots(shots), deviation(deviation), accuracy(accuracy), range(range), ammoType(ammoType), shot_delay(shot_delay), aoe(area) {};
 	bool 		reload(unsigned numberOfAmmo);
 	bool		canFire() const;
 	//TODO: combine fire and deviate into single function
@@ -85,7 +86,8 @@ public:
 
 class Knife : public Weapon {
 public:
-	Knife() : Weapon(Icon_Weapon_Knife, "Throwing Knife", 5, 500, 22, 1, 1, 0, 6, Ammo_Knife) { loadedAmmo = 5; }
+	Knife() : Weapon(Icon_Weapon_Knife, "Throwing Knife", true, false, 5, 5, 500, 22, 1, 1, 0, 6, Ammo_Knife) {}
+	virtual int fire() { if (this->getAmount() >= 1) { this->removeAmount(1); } return 1; };
 };
 
 class RocketLauncher : public Weapon {
@@ -95,7 +97,8 @@ public:
 
 class Grenade : public Weapon {
 public:
-	Grenade(): Weapon(Icon_Weapon_Grenade, "Grenade", 6, 500, 20, 1, 1, 0, 7, Ammo_Grenades, 0, 1) { loadedAmmo = 2; }
+	Grenade(): Weapon(Icon_Weapon_Grenade, "Grenade", true, false, 2, 6, 500, 20, 1, 1, 0, 7, Ammo_Grenades, 0, 1) {}
+	virtual int fire() { if (this->getAmount() >= 1) { this->removeAmount(1); } return 1;} ;
 };
 	
 
