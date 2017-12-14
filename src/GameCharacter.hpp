84289b85e35d28056d8080086f5a9cde011ce267
Tommi Räsänen <tommi.rasanen@aalto.fi>
2017-12-14 23:10:09 +0200

    Increase GameCharacter default LOS to 7 tiles in each direction

--- src/GameCharacter.hpp
+++ src/GameCharacter.hpp
@@ -109,7 +109,7 @@ private:
 	unsigned int maxHealth = 40;
 	std::shared_ptr<Weapon> equippedWeapon = std::make_shared<Hands>();
 	unsigned int team;
-    unsigned int lengthofSight = 9;
+    unsigned int lengthofSight = 15;
 	int selectedItemIdx = -1;
 	int selectedWeaponIdx = -1;
 	bool remove = false;
